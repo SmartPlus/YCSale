c348c043f304d8727f125c6a123687dfc46716f6
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2015-03-08 01:36:04 +0700

    update api

--- main.go
+++ main.go
@@ -1,57 +1,35 @@
 package main
 
 import (
-	"YCSale/database"
-	"YCSale/security"
 	"YCSale/service"
+	// "YCSale/security"
 	"YCSale/session"
-	"encoding/json"
+	"YCSale/web"
+	// 	"encoding/json"
 	"github.com/go-martini/martini"
 	"github.com/martini-contrib/render"
-	"net/http"
+	// "net/http"
 )
-
-type Course struct {
-	Id   string
-	Name string
-	Cost int
-}
-
-type Payment struct {
-	Id      string
-	Course  string
-	Learner string
-	Ammount int
-}
 
 const (
-	MONGO_USERNAME            = "debug" //"dong"
-	MONGO_PASSWORD            = "123"
-	MONGO_HOST                = "localhost" //"ds031271.mongolab.com"
-	MONGO_PORT                = 27017       //31271
-	MONG_DB_NAME              = "smartplus"
-	WISHES_COLLECTION_NAME    = "wishes"
-	USERS_COLLECTION_NAME     = "users"
-	CUSTOMERS_COLLECTION_NAME = "customers"
-	COURSES_COLLECTION_NAME   = "courses"
-	COOKIE_SECRET             = "cki"
-	COOKIE_KEY                = "my_session"
-	TEMPLATE_DIRECTORY        = "templates"
-	TEMPLATE_LAYOUT           = "layout"
-	ADMIN_PAGE                = "admin"
+	COOKIE_SECRET      = "cki"
+	COOKIE_KEY         = "my_session"
+	TEMPLATE_DIRECTORY = "templates"
+	TEMPLATE_LAYOUT    = "layout"
+	ADMIN_PAGE         = "admin"
 )
 
-func Authenticate() martini.Handler {
-	return func(res http.ResponseWriter, req *http.Request, context martini.Context) {
-		decoder := json.NewDecoder(req.Body)
-		err := decoder.Decode(&security.SessionUser{})
-		if err != nil {
-			http.Error(res, "Unauthorized", http.StatusUnauthorized)
-			return
-		}
-		context.Next()
-	}
-}
+// func Authenticate() martini.Handler {
+// 	return func(res http.ResponseWriter, req *http.Request, context martini.Context) {
+// 		decoder := json.NewDecoder(req.Body)
+// 		err := decoder.Decode(&security.SessionUser{})
+// 		if err != nil {
+// 			http.Error(res, "Unauthorized", http.StatusUnauthorized)
+// 			return
+// 		}
+// 		context.Next()
+// 	}
+// }
 
 func main() {
 	m := martini.Classic()
@@ -65,19 +43,10 @@ func main() {
 		Extensions: []string{".html"},
 	}))
 
-	if err := database.Init(MONGO_USERNAME, MONGO_PASSWORD, MONGO_HOST, MONGO_PORT, MONG_DB_NAME); err != nil {
-		panic(err)
-	}
+	m.Use(service.DbMartiniHandler())
 
-	service.Init(m, map[string]string{
-		"wish":     WISHES_COLLECTION_NAME,
-		"user":     USERS_COLLECTION_NAME,
-		"customer": CUSTOMERS_COLLECTION_NAME,
-		"course":   COURSES_COLLECTION_NAME,
-	})
+	web.Init(m)
 
-	// security use service
-	security.Init(m)
 	m.Get("/admin", func(r render.Render) {
 		r.HTML(200, ADMIN_PAGE, map[string]string{
 			"Title": "Admin",
