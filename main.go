4a9016a8d19b1fc964d69e03e23d74748b7faa19
Nguyễn Trường Minh <nguyentruongminh7@gmail.com>
2015-03-09 20:24:20 +0700

    port in command line

--- main.go
+++ main.go
@@ -1,59 +1,15 @@
 package main
 
 import (
-	"YCSale/service"
-	"YCSale/service/session"
 	"YCSale/web"
-	// 	"encoding/json"
-	"github.com/go-martini/martini"
-	"github.com/martini-contrib/render"
-	// "net/http"
+	"flag"
+	"fmt"
 )
 
-const (
-	TEMPLATE_DIRECTORY = "templates"
-	TEMPLATE_LAYOUT    = "layout"
-	ADMIN_PAGE         = "admin"
-)
-
-// func Authenticate() martini.Handler {
-// 	return func(res http.ResponseWriter, req *http.Request, context martini.Context) {
-// 		decoder := json.NewDecoder(req.Body)
-// 		err := decoder.Decode(&security.SessionUser{})
-// 		if err != nil {
-// 			http.Error(res, "Unauthorized", http.StatusUnauthorized)
-// 			return
-// 		}
-// 		context.Next()
-// 	}
-// }
+var PORT int
 
 func main() {
-	m := martini.Classic()
-	/* global middleware */
-	m.Use(martini.Recovery())
-	m.Use(martini.Static("www"))
-	m.Use(render.Renderer(render.Options{
-		Directory:  TEMPLATE_DIRECTORY,
-		Layout:     TEMPLATE_LAYOUT,
-		Extensions: []string{".html"},
-	}))
-
-	m.Use(session.Session())
-
-	m.Use(service.DbMartiniHandler())
-	web.Init(m)
-
-	m.Get("/admin", func(r render.Render) {
-		r.HTML(200, ADMIN_PAGE, map[string]string{
-			"Title": "Admin",
-		})
-	})
-	m.Get("/", func(r render.Render) {
-		r.HTML(200, "index", map[string]string{
-			"Title": "YC Sale",
-		})
-	})
-
-	m.RunOnAddr(":8080")
+	flag.IntVar(&PORT, "port", 8080, "port number")
+	flag.Parse()
+	web.Init().RunOnAddr(fmt.Sprintf(":%v", PORT))
 }
